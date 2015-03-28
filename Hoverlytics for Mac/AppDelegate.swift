61a47f2a66a9ac80c235affc787f38d35e35bbba
Patrick Smith <pgwsmith@gmail.com>
2015-03-28 21:25:14 +1100

    Initial Commit

---
+++ Hoverlytics for Mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hoverlytics for Mac
+//
+//  Created by Patrick Smith on 28/03/2015.
+//  Copyright (c) 2015 Burnt Caramel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
