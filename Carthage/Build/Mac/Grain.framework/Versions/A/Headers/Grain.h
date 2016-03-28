cd18ddd0a6b7e7b0df5b71ee8d1859b8aa2fa465
Patrick Smith <pgwsmith@gmail.com>
2016-03-28 17:17:55 +1100

    Add Grain dependency

---
+++ Carthage/Build/Mac/Grain.framework/Versions/A/Headers/Grain.h
@@ -0,0 +1,19 @@
+//
+//  Grain.h
+//  Grain
+//
+//  Created by Patrick Smith on 17/03/2016.
+//  Copyright © 2016 Burnt Caramel. All rights reserved.
+//
+
+@import Foundation;
+
+//! Project version number for Grain.
+FOUNDATION_EXPORT double GrainVersionNumber;
+
+//! Project version string for Grain.
+FOUNDATION_EXPORT const unsigned char GrainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Grain/PublicHeader.h>
+
+
